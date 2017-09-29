8451b80b27e97165b4e2fdadef102df294546477
Miftah Mizwar <miftahmizwar@gmail.com>
2017-09-29 22:04:59 +0700

    first commit

--- RollTheDices/ViewController.swift
+++ RollTheDices/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rightDiceView: UIImageView!
+    @IBOutlet weak var leftDiceView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollButtonTapped(_ sender: Any) {
+        let randomNumber = Int(arc4random_uniform(5)+1)
+        leftDiceView.image = UIImage(named: "dieWhite_border\(randomNumber)")
+        
+        let randomNumber2 = Int(arc4random_uniform(5)+1)
+        rightDiceView.image = UIImage(named: "dieWhite_border\(randomNumber2)")
+    }
+    
 }
 
